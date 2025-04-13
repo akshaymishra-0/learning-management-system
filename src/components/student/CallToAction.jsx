2d4c17902aeb5364d89128ef437539c2582e3b84
akshaymishra-0 <studentakshay2004@gmail.com>
2025-04-13 14:40:30 +0530

    first commit

---
+++ src/components/student/CallToAction.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CallToAction = () => {
+  return (
+    <div><h1>Call To action</h1></div>
+  )
+}
+
+export default CallToAction
---
+++ src/components/student/TestimonialsSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TestimonialsSection = () => {
+  return (
+    <div><h1>Testimonial Section</h1></div>
+  )
+}
+
+export default TestimonialsSection
